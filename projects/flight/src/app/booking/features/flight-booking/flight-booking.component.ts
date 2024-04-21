04368d706c59a703d65e79b2a49a18343fc6f3d0
mikezks <23002661+mikezks@users.noreply.github.com>
2024-04-21 21:13:02 +0200

    feat: directives and di

--- projects/flight/src/app/booking/features/flight-booking/flight-booking.component.ts
+++ projects/flight/src/app/booking/features/flight-booking/flight-booking.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '../../../shared/ui-common';
 import { provideNavigationService } from '../../../shared/logic-navigation';
+import { NavigationProviderDirective } from '../../../shared/ui-common/navigation/navigation-provider.directive';
 
 
 @Component({
@@ -9,20 +10,23 @@ import { provideNavigationService } from '../../../shared/logic-navigation';
   standalone: true,
   imports: [
     RouterOutlet,
-    NavigationComponent
+    NavigationComponent, NavigationProviderDirective
   ],
   providers: [
     provideNavigationService([
-      { route: 'flight/search', label: 'Flight Search', icon: 'flight' },
-      { route: 'flight/departures', label: 'Departures', icon: 'departures' }
+      { route: 'flight/search', label: 'Flight Search', icon: 'flight' }
     ])
   ],
-
   template: `
     <app-navigation class="nav-standalone" />
+    <app-navigation [navConfig]="navConfig" class="nav-standalone" />
     <div>
       <router-outlet></router-outlet>
     </div>
   `
 })
-export class FlightBookingComponent {}
+export class FlightBookingComponent {
+  navConfig = [
+    { route: 'flight/departures', label: 'Departures', icon: 'departures' }
+  ];
+}
